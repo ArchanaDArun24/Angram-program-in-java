e2dbd4bd0ca7cc6ade91dd9de3cc3274b1816599
Archana D Arun <83198815+ArchanaDArun24@users.noreply.github.com>
2021-09-03 16:18:50 +0530

    Add files via upload

---
+++ angramProgram.java
@@ -0,0 +1,14 @@
+package String;
+import java.util.*;
+public class angramProgram {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String x=sc.nextLine();
+		String y=sc.nextLine();
+		x=x.replace(" ", "");
+		y=y.replace(" ", "");
+		x=x.toUpperCase();
+		y=y.toUpperCase();
+		
+	}
+}
